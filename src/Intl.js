0c3dac4fcf527ab1d886f3441c60c6fa2dac5f7f
Kai Hao <kevin830726@gmail.com>
2018-02-08 14:57:36 +0800

    cache formatter

--- src/Intl.js
+++ src/Intl.js
@@ -1,5 +1,6 @@
 import IntlMessageFormat from 'intl-messageformat';
 import IntlRelativeFormat from 'intl-relativeformat';
+import memoizeIntlConstructor from 'intl-format-cache';
 
 const toDate = value => value instanceof global.Date ? value : new Date(value);
 
@@ -7,6 +8,13 @@ class Intl {
   constructor(locale, messages) {
     this.locale = locale;
     this.messages = messages;
+
+    this.formatters = {
+      getDateTimeFormat: memoizeIntlConstructor(global.Intl.DateTimeFormat),
+      getNumberFormat: memoizeIntlConstructor(global.Intl.NumberFormat),
+      getMessageFormat: memoizeIntlConstructor(IntlMessageFormat),
+      getRelativeFormat: memoizeIntlConstructor(IntlRelativeFormat),
+    };
   }
 
   shouldUseFallback = (id) => {
@@ -16,21 +24,21 @@ class Intl {
   formatMessage = ({ id, defaultMessage = '' }, values = {}) => {
     if (this.shouldUseFallback(id)) {
       // no message found and fallback to default message
-      return new IntlMessageFormat(defaultMessage, this.locale).format(values);
+      return this.formatters.getMessageFormat(defaultMessage, this.locale).format(values);
     }
 
     // message found, format and return
-    const intlMessage = new IntlMessageFormat(this.messages[this.locale][id], this.locale);
+    const intlMessage = this.formatters.getMessageFormat(this.messages[this.locale][id], this.locale);
     return intlMessage.format(values);
   }
 
   formatNumber = (value, options = {}) => {
-    return new global.Intl.NumberFormat(this.locale, options).format(value);
+    return this.formatters.getNumberFormat(this.locale, options).format(value);
   }
 
   formatDate = (value, options = {}) => {
     const date = toDate(value);
-    return new global.Intl.DateTimeFormat(this.locale, options).format(date);
+    return this.formatters.getDateTimeFormat(this.locale, options).format(date);
   }
 
   formatTime = (value, options = {}) => {
@@ -43,7 +51,7 @@ class Intl {
 
   formatRelative = (value, options = {}) => {
     const date = toDate(value);
-    const intlRelative = new IntlRelativeFormat(this.locale, options);
+    const intlRelative = this.formatters.getRelativeFormat(this.locale, options);
     return intlRelative.format(date);
   }
 }
