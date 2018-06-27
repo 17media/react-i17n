3274ef667e97ab659738613e646ed9e38d98b2a7
Kai Hao <kevin830726@gmail.com>
2018-06-28 00:27:44 +0800

    add es module export

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,61 @@ import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 import replace from 'rollup-plugin-replace';
 
-export default {
+const base = {
   input: 'src/index.js',
-  output: {
-    file: 'dist/react-i17n.min.js',
-    format: 'umd',
-    name: 'ReactI17n',
-    globals: 'ReactI17n',
-    sourcemap: true,
-  },
-  plugins: [
-    commonjs({
-      include: 'node_modules/**',
-    }),
-    resolve({
-      jsnext: true,
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    uglify({
-      warnings: false,
-    }),
-  ],
-  external: ['react'],
 };
+
+export default [
+  // umd
+  {
+    ...base,
+    output: {
+      file: 'dist/react-i17n.min.js',
+      format: 'umd',
+      name: 'ReactI17n',
+      globals: 'ReactI17n',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      resolve({
+        jsnext: true,
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      uglify({
+        warnings: false,
+      }),
+    ],
+    external: ['react'],
+  },
+  // es and cjs
+  {
+    ...base,
+    output: [
+      {
+        file: 'lib/react-i17n.es.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: 'lib/react-i17n.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [babel()],
+    external: [
+      'react',
+      'intl-format-cache',
+      'intl-messageformat',
+      'intl-relativeformat',
+    ],
+  },
+];
